b58d954b6ba669ae3d9ed49c634cc15192509956
diegoargottez@gmail.com <diegoargottez@gmail.com>
2022-10-14 15:03:47 -0400

    windows form incluido

---
+++ WINDOWSFORMPRUEBA/Form1.cs
@@ -0,0 +1,36 @@
+﻿using ServiceOrion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WINDOWSFORMPRUEBA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string id;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Este ID es para materiales.
+            id = "5090001";
+            Calls call = new Calls(id);
+            var response = call.ObtenerMaterial().Name;
+            MessageBox.Show(response);
+
+            Calls call1 = new Calls();
+            var response1 = call1.ObtenerListaMaterial();
+            id = "aaaaaaaaaaaaaaaaaaa";
+        }
+    }
+}
